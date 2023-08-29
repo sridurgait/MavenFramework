8685d859add15c830145b39ec0f66dd7b2c93c7d
sridurgait <Lenovo@DESKTOP-KHV87JG>
2023-08-29 10:06:21 +0530

    1st commit

---
+++ Maven_Demo/src/test/java/Pratice/DemoTest.java
@@ -0,0 +1,20 @@
+package Pratice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoTest {
+	@Test
+ public void DemoTest()
+ {
+		
+		System.out.println("hi");
+	 System.out.println("Git Pull Back");
+	 
+ }
+}
